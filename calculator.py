797e24540934529d458b370374b032ae2ea3da04
yashodagit <60279357+yashodagit@users.noreply.github.com>
2024-02-18 22:29:34 -0600

    sucess

---
+++ calculator.py
@@ -0,0 +1,10 @@
+print("calculator app")
+first = float(input("enter the first number "))
+
+second = float(input("enter the second number "))
+sum = str(first+second)
+print("sum is" + sum)
+
+
+
+
